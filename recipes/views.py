89f731be36d4d5dfc34f6bd13989cd0502f3005e
soraiapacheco <soraiapas@gmail.com>
2023-04-21 11:14:05 +0100

    put data in the card of recipes: creating factory.py to generate data fakers

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Soraia Felício',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Soraia Felício',
+        'recipe': make_recipe(),
     })
