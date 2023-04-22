61cd61be9528e2089f82040eac775c3a706dd1a6
soraiapacheco <soraiapas@gmail.com>
2023-04-22 11:15:51 +0100

    creating more one table in the models and running of migration to create the 2 tables: category and recipe

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class Recipe(models.Models):
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug = models.SlugFields()
+    slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
@@ -15,3 +20,7 @@ class Recipe(models.Models):
     update_at = models.DateTimeField(auto_now=True)
     is_publisched = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
