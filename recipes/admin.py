94186057ae569f6f0c7817b1bf41aad6136462fb
soraiapacheco <soraiapas@gmail.com>
2023-04-22 12:51:26 +0100

    Register the recipe table and insert some data in the tables (recipes and category)

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 # Register your models here.
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    a = ''
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
 
 
 admin.site.register(Category, CategoryAdmin)
