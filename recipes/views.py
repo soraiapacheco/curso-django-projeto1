58dfc3dc1007173fb85cc8284e0d955c7b520277
soraiapacheco <soraiapas@gmail.com>
2023-04-23 14:13:58 +0100

    adding filter is_published in the recipes

--- recipes/views.py
+++ recipes/views.py
@@ -5,15 +5,16 @@ from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_publisched=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_publisched=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
