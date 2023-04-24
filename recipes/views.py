eaaa186f95efbb3786e5dd4375ab4c0ba25d175c
soraiapacheco <soraiapas@gmail.com>
2023-04-24 10:12:59 +0100

    adjusting how to deal with 404 error using get_list_or_404 function of Django

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
@@ -13,14 +12,18 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id, is_published=True).order_by('-id')
 
-    if not recipes:
-        raise Http404('Not found!')
+    # if not recipes:
+    #     raise Http404('Not found!')
+
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category |',
+        'title': f'{recipes[0].category.name} - Category |',
     })
 
 
