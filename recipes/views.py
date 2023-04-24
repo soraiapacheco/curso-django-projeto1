1bfe550905d6a095fdf5ec13800ab3b5bdd37f70
soraiapacheco <soraiapas@gmail.com>
2023-04-24 11:18:04 +0100

    adjusting static.css for home.html files if not exists recipes published. It was static.css of the gitignore to upload

--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,7 @@ from utils.recipes.factory import make_recipe
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
