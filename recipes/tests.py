3c5ff4c016dd534188f497d648e5aae8d30cc334
soraiapacheco <soraiapas@gmail.com>
2023-04-25 13:04:13 +0100

    test of url home of recipes app

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 # Create your tests here.
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        variavel = 123456
-        print('Olá mundo!')
-        assert 1 == 1, 'Um  é igual a um'
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
--- recipes/urls.py
+++ recipes/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 app_name = 'recipes'
 urlpatterns = [
-    path('', views.home, name='recipes-home'),  # home
+    path('', views.home, name='home'),  # home
     path('recipes/category/<int:category_id>/',
          views.category, name='category'),  # recipe
     path('recipes/<int:id>/', views.recipe, name='recipe'),  # recipe
