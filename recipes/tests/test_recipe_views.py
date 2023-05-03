7bb0cafeafb636121f143b264b562c53ae2f6272
soraiapacheco <soraiapas@gmail.com>
2023-05-03 17:38:47 +0100

    example with comment of the fail and skip functions

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,16 +1,17 @@
+from unittest import skip
+
 from django.urls import resolve, reverse
 
 from recipes import views
 
-from .test_recipe_base import Recipe, RecipeTestBase
+from .test_recipe_base import RecipeTestBase
 
 
+# para pular os testes
+# @skip('mensagem porque eu estou pulando esses testes')
 class RecipeViewsTest(RecipeTestBase):
 
-    def tearDown(self) -> None:
-        return super().tearDown()
-
-    # setUp is runned before of each test
+  # setUp is runned before of each test
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -24,11 +25,17 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
+    # WIP = Wokr in progress
+    # @skip('WIP')
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertIn(
             '<h1>No recipes found here!</h1>',
             response.content.decode('utf-8'))
+
+        # Supose that I write more test and I want to stop here
+        # I can cause the fail function of the test, for example
+        # self.fail('Para eu terminar de digitar o que falta')
 
     def test_recipe_home_templates_loads_recipes(self):
         # Other way to create category, but it is necessary to include the command save()
