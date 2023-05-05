7de39fb3f110021fefd2c39b9d3ccdfd20ad0088
soraiapacheco <soraiapas@gmail.com>
2023-05-05 21:46:29 +0100

    including parameterized package to create several test for fields of recipe table with the same properties

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
 from .test_recipe_base import RecipeTestBase
 
@@ -8,22 +9,12 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_recipe_title_raise_error_if_title_more_than_65_chars(self):
-        self.recipe.title = 'A' * 70
+    @parameterized.expand([('title', 65),
+                           ('description', 165),
+                           ('preparation_time_unit', 65),
+                           ('servings_unit', 65)])
+    def test_recipe_max_length(self, field, max_length):
+        # attribution dynamic for recipe variable
+        setattr(self.recipe, field, 'A' * (max_length+1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
-
-    def test_recipe_max_length(self):
-        fields = [
-            ('title', 65),
-            ('description', 165),
-            ('preparation_time_unit', 65),
-            ('servings_unit', 65)
-
-        ]
-        for field, max_length in fields:
-            with self.subTest(field=field, max_length=max_length):
-                # attribution dynamic for recipe variable
-                setattr(self.recipe, field, 'A' * (max_length+1))
-                with self.assertRaises(ValidationError):
-                    self.recipe.full_clean()
