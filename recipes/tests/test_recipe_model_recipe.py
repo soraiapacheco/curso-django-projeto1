84cfe0e96324ed4ba75a65c2c1ffc00b82af3026
soraiapacheco <soraiapas@gmail.com>
2023-05-08 18:38:52 +0100

    including test_recipe_is_published_is_false_by_default function and adjusting test_recipe_preparation_steps_is_html_is_false_by_default creating the make_recipe_no_defaults that used by both functions

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -9,17 +9,7 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    @parameterized.expand([('title', 65),
-                           ('description', 165),
-                           ('preparation_time_unit', 65),
-                           ('servings_unit', 65)])
-    def test_recipe_max_length(self, field, max_length):
-        # attribution dynamic for recipe variable
-        setattr(self.recipe, field, 'A' * (max_length+1))
-        with self.assertRaises(ValidationError):
-            self.recipe.full_clean()
-
-    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+    def make_recipe_no_defaults(self):
         recipe = Recipe(
             category=self.make_category(name='Test Default Category'),
             author=self.make_author(username='newuser'),
@@ -34,5 +24,24 @@ class RecipeModelTest(RecipeTestBase):
         )
         recipe.full_clean()
         recipe.save()
+        return recipe
+
+    @parameterized.expand([('title', 65),
+                           ('description', 165),
+                           ('preparation_time_unit', 65),
+                           ('servings_unit', 65)])
+    def test_recipe_max_length(self, field, max_length):
+        # attribution dynamic for recipe variable
+        setattr(self.recipe, field, 'A' * (max_length+1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
         self.assertFalse(recipe.preparation_steps_is_html,
                          msg='Recipe preparation_steps_is_html is not False',)
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(recipe.is_published,
+                         msg='Recipe is_published is not False',)
