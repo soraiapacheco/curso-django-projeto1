c544133fc3a166f62f4b12bcb8dfa6a3acb10cbc
soraiapacheco <soraiapas@gmail.com>
2023-05-15 14:03:02 +0100

    adjusting the pagination range test TDD

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,2 +1,15 @@
+import math
+
+
 def make_pagination_range(page_range, qty_pages, current_page):
-    return [1, 2, 3, 4]
+    middle_page = math.ceil(qty_pages/2)  # around for above
+    start_range = current_page - middle_page
+    stop_range = current_page + middle_page
+
+    start_range_offset = abs(start_range) if start_range < 0 else 0
+
+    if (start_range < 0):
+        start_range = 0
+        stop_range += start_range_offset
+
+    return page_range[start_range:stop_range]
