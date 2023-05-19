1bb5b942a1a387a071d9107d4cce882e3dfd59d2
soraiapacheco <soraiapas@gmail.com>
2023-05-19 15:36:11 +0100

    setting within forms the labels, errors messages, widgets and field attributes

--- authors/forms.py
+++ authors/forms.py
@@ -11,5 +11,36 @@ class RegisterForm(forms.ModelForm):
                   'username',
                   'password',
                   'email',
-
                   ]
+        # alter for all fields except first_name for example
+        # exclude = ['first_name']
+
+        # Setting the labels
+        labels = {
+            'username': 'Username',
+            'first_name':  'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+
+        help_texts = {
+            'email': 'The e-mail must be valid.',
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty.',
+                'invalid': 'This field is invalid.',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here.',
+                'class': 'input text-input outra-classe',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here.',
+            })
+        }
