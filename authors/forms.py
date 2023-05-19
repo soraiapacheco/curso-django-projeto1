b57629a15cb7f4477abc443b31b56c53b17b611b
soraiapacheco <soraiapas@gmail.com>
2023-05-19 11:00:12 +0100

    creating first form using Django form

---
+++ authors/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        # for all fiels use: fields = '__all__'
+        fields = ['first_name',
+                  'last_name',
+                  'username',
+                  'email',
+
+                  ]
