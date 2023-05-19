47b9b5a2ea66259e5339d9adf487a73b6ed2e5e8
soraiapacheco <soraiapas@gmail.com>
2023-05-19 10:50:19 +0100

    creating new app call authors to register the users

--- project/settings.py
+++ project/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes'
+    # my apps
+    'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
