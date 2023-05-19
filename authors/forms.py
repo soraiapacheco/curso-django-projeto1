9fee40a29286ccf1c4fa5e9c9a7d83d21015fec3
soraiapacheco <soraiapas@gmail.com>
2023-05-19 16:00:16 +0100

    improving the __init__ function with 2 ways to add characterists to fields

--- authors/forms.py
+++ authors/forms.py
@@ -2,11 +2,24 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['placeholder'] = 'Que legal'
+        # add_attr(self.fields['username'], 'placeholder', 'Your username')
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your email')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
 
     password = forms.CharField(
         required=True,
