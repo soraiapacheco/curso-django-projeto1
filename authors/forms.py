376c724d9024e8ba8c019674ba6956d686540802
soraiapacheco <soraiapas@gmail.com>
2023-05-20 11:32:49 +0100

    including method clean to many fields validation

--- authors/forms.py
+++ authors/forms.py
@@ -110,3 +110,16 @@ class RegisterForm(forms.ModelForm):
             )
 
         return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError({
+                'password': 'Password and password2 must be equal.',
+                'password2': 'Password and password2 must be equal.',
+            }
+            )
