2e2a9cbb1d0b023965aa0396e329ae41869346b1
soraiapacheco <soraiapas@gmail.com>
2023-05-22 18:08:35 +0100

    adding test of pagination in home and category and adjusting test get request registration in author form. Now, coverage of the test is 100%

--- recipes/tests/test_recipe_category_view.py
+++ recipes/tests/test_recipe_category_view.py
@@ -1,5 +1,7 @@
 # from unittest import skip
 
+from unittest.mock import patch
+
 from django.urls import resolve, reverse
 
 from recipes import views
@@ -38,3 +40,26 @@ class RecipeCategoryViewsTest(RecipeTestBase):
         response = self.client.get(
             reverse('recipes:category', kwargs={'category_id': 1000}))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_category_view_is_paginated(self):
+        for i in range(8):
+            kwargs = {'author_data': {'username': f'u{i}'}, 'slug': f'r{i}'}
+            self.make_recipe(**kwargs)
+
+        # substituted by patch of mock (imported by unitest)
+        # the part of the code below can change the PER_PAGE for all tests
+        # So this code below is not a good practice of programmation
+        # import recipes
+        # simulating the per_page value
+        # recipes.views.PER_PAGE = 3
+
+        # patch as context management
+        with patch('recipes.views.PER_PAGE', new=3):
+            response = self.client.get(
+                reverse('recipes:category', kwargs={'category_id': 1}))
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+            # verify the amount of pages is equal to number of pages
+            # should have in the total
+            self.assertEqual(paginator.num_pages, 1)
