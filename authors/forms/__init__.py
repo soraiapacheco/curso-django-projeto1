16fd66f11d88ba2889ad0d1840ea9c07f8ad589b
soraiapacheco <soraiapas@gmail.com>
2023-05-24 12:56:55 +0100

    creating the forms of login

--- authors/forms/__init__.py
+++ authors/forms/__init__.py
@@ -2,4 +2,5 @@
 # flake8:noqa
 # The Django that the form folder contains the RegisterForm.py
 # So the app not broke due to this feature
+from .login import LoginForm
 from .register_form import RegisterForm
