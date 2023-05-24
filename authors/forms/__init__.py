171c5ea80e6c1f4cf1257f39b382015d32e317a3
soraiapacheco <soraiapas@gmail.com>
2023-05-24 12:43:53 +0100

    refactoring form moving the file form.py to folder called form. However this file called register_form.py. Creating the __init__.py within this form folder and importing the RegisterForm class. So the form folder behavior as a file called form.py that contains the RegisterForm class. All app continues running correctly

---
+++ authors/forms/__init__.py
@@ -0,0 +1,5 @@
+# desactive missing use of importation below
+# flake8:noqa
+# The Django that the form folder contains the RegisterForm.py
+# So the app not broke due to this feature
+from .register_form import RegisterForm
