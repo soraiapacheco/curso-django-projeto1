1ce1492ba2144ad7a96164e3f8369c40c8933214
soraiapacheco <soraiapas@gmail.com>
2023-05-25 15:58:21 +0100

    organizing the files of test functional

---
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -0,0 +1,12 @@
+
+from selenium.webdriver.common.by import By
+
+from .base import RecipeBaseFunctionalTest
+
+
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+
+    def test_recipe_home_without_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('No recipes found here!', body.text)
