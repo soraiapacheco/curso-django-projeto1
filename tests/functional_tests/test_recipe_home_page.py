3c40e25b019c8e4d84adca96c4ec27c2751f54bd
soraiapacheco <soraiapas@gmail.com>
2023-05-25 15:44:51 +0100

    refactoring class tests functional to open and close the browse when it runs each test

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -6,13 +6,25 @@ from selenium.webdriver.common.by import By
 from utils.browser import make_chrome_browser
 
 
-class RecipeHomePageFunctionTest(StaticLiveServerTestCase):
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+        return super().tearDown()
+
     def sleep(self, seconds=5):
         time.sleep(seconds)
 
+
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+
     def test_the_test(self):
-        browser = make_chrome_browser()
+        browser = self.browser
         browser.get(self.live_server_url)
         body = browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here!', body.text)
-        browser.quit()
