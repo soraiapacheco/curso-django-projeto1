9cedc216ad8403f8cc9af28a5ea1e43d043b2e2f
soraiapacheco <soraiapas@gmail.com>
2023-05-25 15:40:33 +0100

    refactoring first test selenium adding comparation of contain within the page

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -1,6 +1,7 @@
 import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
 
 from utils.browser import make_chrome_browser
 
@@ -12,5 +13,6 @@ class RecipeHomePageFunctionTest(StaticLiveServerTestCase):
     def test_the_test(self):
         browser = make_chrome_browser()
         browser.get(self.live_server_url)
-        self.sleep(6)
+        body = browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('No recipes found here!', body.text)
         browser.quit()
