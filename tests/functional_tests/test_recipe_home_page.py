d224173d18cdde1fa969aea8631cc5ee81353abe
soraiapacheco <soraiapas@gmail.com>
2023-05-25 14:57:02 +0100

    adding first test selenium without static files

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -1,6 +1,16 @@
+import time
+
 from django.test import LiveServerTestCase
+
+from utils.browser import make_chrome_browser
 
 
 class RecipeHomePageFunctionTest(LiveServerTestCase):
+    def sleep(self, seconds=5):
+        time.sleep(seconds)
+
     def test_the_test(self):
-        assert 1 == 1
+        browser = make_chrome_browser()
+        browser.get(self.live_server_url)
+        self.sleep(6)
+        browser.quit()
