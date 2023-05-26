934ef95cd7fdfbc4a493b4a886d7282725acf8fd
soraiapacheco <soraiapas@gmail.com>
2023-05-26 14:15:24 +0100

    sending the content to input field

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -1,7 +1,22 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 from .base import AuthorBaseTest
 
 
 class AuthorsRegisterTest(AuthorBaseTest):
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(By.XPATH,
+                                        f'//input[@placeholder="{placeholder}"]')
+
     def test_the_test(self):
         self.browser.get(self.live_server_url + '/authors/register')
-        self.sleep()
+        form = self.browser.find_element(
+            By.XPATH,
+            '/html/body/main/div[2]/form'
+        )
+        first_name_field = self.get_by_placeholder(form, 'Ex.: John')
+        first_name_field.send_keys('ATENÇÃO')
+        first_name_field.send_keys(Keys.ENTER)
+
+        self.sleep(5)
