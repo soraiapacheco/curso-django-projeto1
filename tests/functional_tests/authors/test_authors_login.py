6c6789dfa49fb98aaf84dae5031817ca9abfc902
soraiapacheco <soraiapas@gmail.com>
2023-05-27 11:38:44 +0100

    adding functional test invalid credential user

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -47,7 +47,7 @@ class AuthorsLoginTest(AuthorBaseTest):
                       self.browser.find_element(By.TAG_NAME, 'body').text)
         # self.sleep(6)
 
-    def test_form_login_is_invalid(self):
+    def test_form_login_is_valid(self):
         # User open the login page
         self.browser.get(
             self.live_server_url + reverse('authors:login')
@@ -71,5 +71,32 @@ class AuthorsLoginTest(AuthorBaseTest):
 
         self.assertIn(
             'Invalid username or password',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_form_login_is_invalid_credential(self):
+        # User open the login page
+        self.browser.get(
+            self.live_server_url + reverse('authors:login')
+        )
+
+        # User sees the form
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        # user tries to send empty values with data wrongs
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+
+        username.send_keys('invalid_user')
+        password.send_keys('invalid_password')
+
+        # User submmit the form
+
+        form.submit()
+
+        # User sees msg on the screen
+
+        self.assertIn(
+            'Invalid credentials',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
