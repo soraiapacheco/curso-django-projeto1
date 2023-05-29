9bbb684e3a4d5db80af669fe1b592921f20da694
soraiapacheco <soraiapas@gmail.com>
2023-05-29 13:46:45 +0100

    creating the new recipes in the dashboard of the author

--- authors/views.py
+++ authors/views.py
@@ -122,10 +122,51 @@ def dashboard_recipe_edit(request, id):
 
     form = AuthorRecipeForm(
         request.POST or None,
+        files=request.FILES or None,  # for files of images
         instance=recipe
     )
 
+    if form.is_valid():
+        # form is valid
+        # it grants that the form is salved in recipes but doesn´t send to database
+        # that is done to conclude the form with values valid and it performes the
+        # validation
+        recipe = form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        # now, the form is salved in the database
+        recipe.save()
+        messages.success(request, 'Your recipe has been successfully saved!')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
+
     return render(request, 'authors/pages/dashboard_recipe.html', context={
         'recipe': recipe,
+        'form': form
+    })
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_new(request):
+
+    form = AuthorRecipeForm(
+        request.POST or None,
+        files=request.FILES or None  # for files of images
+    )
+
+    # if the form is valid then save this form
+    if form.is_valid():
+        recipe = form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        # now, the form is salved in the database
+        recipe.save()
+        messages.success(request, 'Your recipe has been successfully created!')
+        return redirect(reverse('authors:dashboard'))
+
+    return render(request, 'authors/pages/dashboard_recipe.html', context={
         'form': form
     })
