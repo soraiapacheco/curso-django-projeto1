50e8bfcdb204b2294223885f39064edf8e9db57a
soraiapacheco <soraiapas@gmail.com>
2023-05-30 11:48:52 +0100

    including automatically stored slug if it doesn't exist

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,9 @@
+from typing import Iterable, Optional
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -35,3 +38,10 @@ class Recipe(models.Model):
 
     def get_absolute_url(self):
         return reverse("recipes:recipe", args=(self.id,))
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.title)}'
+            self.slug = slug
+
+        return super().save(*args, **kwargs)
