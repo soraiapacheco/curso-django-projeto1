20b37ab41e2b8285f0a1efcf6678cd572b1b64a2
soraiapacheco <soraiapas@gmail.com>
2023-05-31 12:27:43 +0100

    adjusting the views of recipes to Class Base View

--- recipes/urls.py
+++ recipes/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 app_name = 'recipes'
 urlpatterns = [
-    path('', views.home, name='home'),  # home
+    path('', views.RecipeListViewBase.as_view(), name='home'),  # home
     path('recipes/search/', views.search, name='search'),  # recipe
     path('recipes/category/<int:category_id>/',
          views.category, name='category'),  # recipe
