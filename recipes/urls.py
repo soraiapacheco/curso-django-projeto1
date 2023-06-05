45ef2354adda78ce6ca25ba0a9d8589331557576
soraiapacheco <soraiapas@gmail.com>
2023-06-05 12:26:06 +0100

    adding JSONResponse to API that it uses Detail Toobar

--- recipes/urls.py
+++ recipes/urls.py
@@ -33,6 +33,9 @@ urlpatterns = [
          name='recipe'),  # recipe
     path('recipes/api/v1/',
          views.RecipeListViewHomeApi.as_view(),
-         name='recipe'),  # recipe
+         name='recipe_api_v1'),  # home API
+    path('recipes/api/v1/<int:pk>/',
+         views.RecipeDetailAPI.as_view(),
+         name='recipe_api_v1_detail'),  # detail API
 
 ]
