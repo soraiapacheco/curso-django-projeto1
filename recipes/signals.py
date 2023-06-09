0810fe0dcbfd0d92e85fb0d21f02f1bfaefb0943
soraiapacheco <soraiapas@gmail.com>
2023-06-09 13:46:30 +0100

    adding signals of delete recipes and delete of images associated

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,6 +1,6 @@
 import os
 
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 
 from recipes.models import Recipe
@@ -16,6 +16,17 @@ def delete_cover(instance):
 
 @receiver(pre_delete, sender=Recipe)
 def recipe_cover_delete(sender, instance, *args, **kwargs):
-    old_instance = Recipe.objects.get(pk=instance.pk)
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
 
     delete_cover(old_instance)
+
+
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+    is_new_cover = (old_instance != instance.cover)
+    # print('OLD INSTANCE:')
+    # print(old_instance)
+    if (is_new_cover) and (old_instance != None):
+        # print('ENTROU AQUI PARA APAGAR!')
+        delete_cover(old_instance)
