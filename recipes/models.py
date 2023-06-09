b26749bf0e5dce92b295f67e6f0db452d2321308
soraiapacheco <soraiapas@gmail.com>
2023-06-09 14:24:18 +0100

    adjusting the width in any image

--- recipes/models.py
+++ recipes/models.py
@@ -71,9 +71,9 @@ class Recipe(models.Model):
         image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
         image_pillow = Image.open(image_full_path)
         original_width, original_height = image_pillow.size
-        if original_width <= new_width:
-            image_pillow.close()
-            return
+        # if original_width <= new_width:
+        #    image_pillow.close()
+        #    return
         new_height = round((new_width * original_height) / original_width)
         new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
 
@@ -93,7 +93,7 @@ class Recipe(models.Model):
         if self.cover:
             try:
 
-                self.resize_image(self.cover, 800)
+                self.resize_image(self.cover, 840)
             except FileNotFoundError:
                 ...
 
