a19ce8db712d64aaba5213b4ec0ba8c1564a80fe
soraiapacheco <soraiapas@gmail.com>
2023-06-09 11:42:09 +0100

    including translation hard code in views with python translation class

--- recipes/views.py
+++ recipes/views.py
@@ -8,6 +8,7 @@ from django.http import JsonResponse
 from django.http.response import Http404
 from django.shortcuts import render
 from django.utils import translation
+from django.utils.translation import gettext as _
 from django.views.generic import DetailView, ListView
 
 from recipes.models import Recipe
@@ -113,10 +114,11 @@ class RecipeListViewCategory(RecipeListViewBase):
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
-
+        category_translation = _('Category')
         ctx.update(
             {
-                'title': f'{ctx.get("recipes")[0].category.name} - Category |',
+                'title': f'{ctx.get("recipes")[0].category.name} - '
+                f'{category_translation} |',
 
             }
         )
